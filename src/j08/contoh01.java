a516afca279cbce01b92ba53141b5c4be966609e
rahul <rahul@LAPTOP-2OQLS66J>
2018-08-02 11:14:06 +0700

    for while do while

---
+++ src/j08/contoh01.java
@@ -0,0 +1,30 @@
+package j08;
+import java.util.Scanner;
+public class contoh01 {
+    public static void main(String[] args)
+    {
+        int x;
+        for(x = 1; x <= 5; x++)
+        {
+            System.out.println("SMK TELKOM MALANG");
+        }
+        
+        //PERULANGAN WHILE
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK TELKOM MALANG");
+            i++;
+        }
+        
+        //PERULANGAN DO WHILE
+        int j =1;
+        do
+        {
+            System.out.println("SMK TELKOM MALANG");
+            j++;
+        }
+        while(j <= 5);
+    }
+    
+}
