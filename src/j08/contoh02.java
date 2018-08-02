334d3c48100aa7ff5403a32ef163403f236e31c8
rahul <rahul@LAPTOP-2OQLS66J>
2018-08-02 12:02:28 +0700

    bilangan 1 - 10

---
+++ src/j08/contoh02.java
@@ -0,0 +1,34 @@
+package j08;
+import java.util.Scanner;
+public class contoh02 {
+    public static void main(String[] args)
+    {
+        int x;
+        
+        for(x = 1; x <= 10; x++)
+        {
+            System.out.print(x + " ");
+        }
+        //PERULANGAN DENGAN WHILE
+        
+        x = 1;
+        
+        while(x <= 10)
+        {
+            System.out.print(x + " ");
+            x++;
+        }
+        
+        //PERULANGAN DENGAN DO WHILE
+        
+        x = 1;
+        do
+        {
+            System.out.append(x + " ");
+            x++;
+        }
+           while (x <= 10);
+        
+    }
+    
+}
